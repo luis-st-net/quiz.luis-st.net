b513111ddd9e602aa4e098b84d3fa8b70a17e5ba
Luis <luis@staudt-fridingen.de>
2025-05-23 18:21:59 +0200

    updated navigation bar

--- src/lib/components/navigation-bar.tsx
+++ src/lib/components/navigation-bar.tsx
@@ -1,29 +1,23 @@
 "use client";
 
 import * as Ui from "@/lib/components/ui/";
-import React, { HTMLAttributes } from "react";
-
-import { cn } from "@/lib/utility";
+import React from "react";
+import Link from "next/link";
 
 export default function NavigationBar() {
 	return (
 		<div className="h-20 flex flex-row items-center p-4 bg-custom-dark-blue">
 			<div className="w-full flex flex-row items-center justify-start gap-4 sm:gap-8 custom-lg:gap-14">
-				<NavigationBarItem title="Quiz"/>
+				<Link href="/">
+					<div className="text-lg text-custom-white-primary xs:text-xl sm:text-2xl custom-lg:text-3xl">
+						<strong>
+							Quiz
+						</strong>
+					</div>
+				</Link>
+			
 			</div>
 			<Ui.ThemeToggle className="hidden nano:block"/>
-		</div>
-	);
-}
-
-function NavigationBarItem(
-	{ title, className, ...props }: HTMLAttributes<HTMLDivElement> & { title: string },
-) {
-	return (
-		<div className={cn("text-lg text-custom-white-primary xs:text-xl sm:text-2xl custom-lg:text-3xl", className)} {...props}>
-			<strong>
-				{title}
-			</strong>
 		</div>
 	);
 }
